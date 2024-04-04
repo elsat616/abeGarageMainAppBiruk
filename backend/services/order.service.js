dff578c81c2a2d9a34e95d63f6da7b9440900651
Biruk-Bogale <bbuurraa10@gmail.com>
2024-04-05 00:14:07 +0300

    orders service

--- backend/services/order.service.js
+++ backend/services/order.service.js
@@ -155,7 +155,9 @@ async function getAllOrderss() {
 
     INNER JOIN order_status ON orders.order_id = order_status.order_id 
 
-    INNER JOIN order_info ON orders.order_id = order_info.order_id`;
+    INNER JOIN order_info ON orders.order_id = order_info.order_id
+    
+    ORDER BY orders.order_id DESC`;
 
     const rows = await connection.query(query1);
 
@@ -173,7 +175,7 @@ async function getsingleOrderr(order) {
   // console.log(order)
   try {
     const query =
-      "SELECT orders.order_id, orders.order_hash, customer_info.customer_first_name, customer_info.customer_last_name, customer_identifier.customer_email, customer_identifier.customer_phone_number, customer_vehicle_info.vehicle_make, employee_info.employee_first_name, employee_info.employee_last_name, order_status.order_status, order_info.additional_request, order_info.additional_requests_completed FROM orders INNER JOIN customer_info ON orders.customer_id = customer_info.customer_id INNER JOIN  customer_identifier ON orders.customer_id = customer_identifier.customer_id INNER JOIN customer_vehicle_info ON orders.vehicle_id = customer_vehicle_info.vehicle_id INNER JOIN employee_info ON orders.employee_id = employee_info.employee_id INNER JOIN order_status ON orders.order_id = order_status.order_id INNER JOIN order_info ON orders.order_id = order_info.order_id WHERE orders.order_hash = ?";
+      "SELECT orders.order_id, orders.order_hash,orders.order_date, customer_info.customer_first_name, customer_info.customer_last_name, customer_info.active_customer_status, customer_identifier.customer_email, customer_identifier.customer_phone_number, customer_vehicle_info.vehicle_make, customer_vehicle_info.vehicle_color,customer_vehicle_info.vehicle_tag,customer_vehicle_info.vehicle_year,customer_vehicle_info.vehicle_mileage,customer_vehicle_info.vehicle_serial, employee_info.employee_first_name, employee_info.employee_last_name, order_status.order_status, order_info.additional_request, order_info.additional_requests_completed FROM orders INNER JOIN customer_info ON orders.customer_id = customer_info.customer_id INNER JOIN  customer_identifier ON orders.customer_id = customer_identifier.customer_id INNER JOIN customer_vehicle_info ON orders.vehicle_id = customer_vehicle_info.vehicle_id INNER JOIN employee_info ON orders.employee_id = employee_info.employee_id INNER JOIN order_status ON orders.order_id = order_status.order_id INNER JOIN order_info ON orders.order_id = order_info.order_id WHERE orders.order_hash = ?";
 
     const rows = await connection.query(query, [order]);
 
